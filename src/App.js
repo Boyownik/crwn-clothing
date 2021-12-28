4e6cabff1659587c8d121faab4b0c4a51de10a58
Boyownik <mrboyownik@gmail.com>
2021-12-28 18:59:57 +0100

    added routing to menu-item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
 import './App.css';
 
-import HomePage from './homepage-component';
+import HomePage from './pages/homepage/homepage-component';
+
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />  
+    <div>
+      <Switch>
+        <div className="App">
+          <Route exact path='/' component={HomePage} /> 
+        </div>
+      </Switch>
     </div>
   );
 }
