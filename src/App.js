4396840cf914da9794d3b9ade198ca1855acdcc3
Boyownik <mrboyownik@gmail.com>
2021-12-30 01:09:15 +0100

    added header component and links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage-component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <div className="App">
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-        </div>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
